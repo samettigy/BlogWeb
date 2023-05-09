689855c917725c31754fbb59443c975c1f156588
103S-7 <103S-7@103S-7>
2023-05-09 09:31:35 +0300

    Proje dosyası ekle.

---
+++ BlogWeb/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlogWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLHAKKIMDATableAdapter dt = new DataSet1TableAdapters.TBLHAKKIMDATableAdapter();
+            Repeater1.DataSource = dt.HakkimdaListele();
+            Repeater1.DataBind();
+        }
+    }
+}
